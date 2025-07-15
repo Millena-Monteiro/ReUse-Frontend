28a6bd0e5397b6ae34866a603844f35e023f313e
Millena-Monteiro <707millenamonteiro@gmail.com>
2025-07-15 20:10:56 -0300

    refactor(structure): reorganize project folders and setup user validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,37 @@
 // Importa a Footer
-import Footer from "../components/footer";
+import Footer from "../components/layout/footer";
 
 // Página principal da ReUse+
 
-      
-      {/* Aqui futuramente vai o Header */}
-      {/* <Header /> */}
+{
+  /* Aqui futuramente vai o Header */
+}
+{
+  /* <Header /> */
+}
 
-      {/* Aqui futuramente vai a seção de boas-vindas (Hero) */}
-      {/* <Hero /> */}
+{
+  /* Aqui futuramente vai a seção de boas-vindas (Hero) */
+}
+{
+  /* <Hero /> */
+}
 
-      {/* Aqui futuramente vai a seção Sobre */}
-      {/* <About /> */}
+{
+  /* Aqui futuramente vai a seção Sobre */
+}
+{
+  /* <About /> */
+}
 
-      {/* Aqui futuramente vão os Benefícios ou Funcionalidades */}
-      {/* <Features /> */}
-    export default function HomePage() {
-    return (
+{
+  /* Aqui futuramente vão os Benefícios ou Funcionalidades */
+}
+{
+  /* <Features /> */
+}
+export default function HomePage() {
+  return (
     <main className="min-h-screen flex flex-col justify-between">
       <Footer />
     </main>
